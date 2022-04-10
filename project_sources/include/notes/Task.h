633c07814756f10b15802df8b46fc33275bc3303
nekita <chubarev.nk@phystech.edu>
2022-04-10 19:42:38 +0300

    Работа с Task

--- project_sources/include/notes/Task.h
+++ project_sources/include/notes/Task.h
@@ -5,18 +5,8 @@
 
 class Task: public Project {
 public:
-    Task(string name, string descr = "", string short_n = "");
-    Task(const vector<Note*>& child, string name, string descr = "", string short_n = "");
+    Task(string name, string descr, string short_name, Aspect* parent, const vector<Note*>& children);
     ~Task();
-    string get_name(char mode = 'a');
-    string get_description(char mode = 'a');
-//    vector<Mark> get_marks();
-    vector<Note> split();
-    string test_note_structure();
-    note_status get_status();
-    void change_status(note_status new_status);
-    float get_bar();
-    void change_bar(float new_bar);
 private:
 };
 
--- project_sources/src/notes/Task.cpp
+++ project_sources/src/notes/Task.cpp
@@ -1,43 +1,10 @@
 #include "Task.h"
 
-Task::Task(string name, string descr, string short_n) :
-Project(name, descr, short_n) {}
+#include <utility>
 
-Task::Task(const vector<Note*>& child, string name, string descr, string short_n) :
-Project(child, name, descr, short_n) {}
+Task::Task(string name, string descr = "", string short_name = "",
+           Aspect* parent = nullptr, const vector<Note*>& children = {}) :
+           Project(std::move(name), std::move(descr),
+                   std::move(short_name), parent, children) {}
 
 Task::~Task() = default;
-
-string Task::get_name(char mode) {
-    return Project::get_name(mode);
-}
-
-string Task::get_description(char mode) {
-    return Project::get_description(mode);
-}
-
-vector<Note> Task::split() {
-//    vector<Note> new_notes = {Todo(this->name_), Todo(this->name_)};
-//    return new_notes;
-}
-
-string Task::test_note_structure() {
-    return "Task test func";
-}
-
-note_status Task::get_status() {
-    return Project::get_status();
-}
-
-void Task::change_status(note_status new_status) {
-    Project::change_status(new_status);
-}
-
-//    vector<Mark> get_marks();
-float Task::get_bar() {
-    return Project::get_bar();
-}
-
-void Task::change_bar(float new_bar) {
-    Project::change_bar(new_bar);
-}
